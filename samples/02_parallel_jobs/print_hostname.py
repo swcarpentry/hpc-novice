7891d6d980f6dadc7a6cc94f1835a9c9deaf4cdf
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-09 15:51:25 +0100

    finished writing simple MPI application that prints the rank, size and hostname

--- samples/02_parallel_jobs/print_hostname.py
+++ samples/02_parallel_jobs/print_hostname.py
@@ -6,11 +6,14 @@ from mpi4py import MPI
 def print_hostname():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
 
     hname = MPI.Get_processor_name()
 
+    print("this is {nrank}/{nsize} running on {hname}".format(nrank=rank,nsize=size,hname=hname))
 
-    print("this is {nrank} running on {hname}".format(nrank=rank,hname=hname))
+    comm.Barrier()
+
 
 if __name__ == '__main__':
 
